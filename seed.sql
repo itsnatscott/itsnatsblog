4ab586044ec7a57ac2b7fea934703c9d2cc6f4ee
Your Name <you@example.com>
2015-04-12 11:59:18 -0400

    made db, schema,seed,and spec

---
+++ seed.sql
@@ -0,0 +1,5 @@
+INSERT INTO posts (title, pic, author, post) VALUES 
+("I made a blog!",
+"https://blog.compete.com/wp-content/uploads/2012/04/Person-Excited-at-Computer.jpg",
+"Nat Scott" , "I've spent all weekend working on making this blog work. I have been swearing and drinking as a result. Please bear with me."); 
+
---
+++ server.js
@@ -0,0 +1,18 @@
+var express = require ('express');
+var app = express();
+var swlite3 = require('sqlite3').verbose;
+var db = new sqlite3.Database('itsnatsblog.db');
+var ejs = require('ejs');
+app.set('view_engine', 'ejs');
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+var methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+app.get('/', function(req,res){
+	res.redirect('/itsnatsblog')
+});
+
+app.get('/itsnatsblog', function(req,res){
+
+})
