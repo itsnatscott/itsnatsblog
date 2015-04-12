e46c72f6c8594239bdd54df9e1982ac00f68e61f
Your Name <you@example.com>
2015-04-12 13:11:49 -0400

    server workingish

--- schema.sql
+++ schema.sql
@@ -1,14 +1,8 @@
 DROP TABLE IF EXISTS posts;
 CREATE TABLE posts (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  id INTEGER PRIMARY KEY,
   title TEXT,
   pic TEXT, 
-  author TEXT,
-  post TEXT, 
-created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+  author TEXT, 
+  post text
 );
-
-CREATE TRIGGER timestamp_update BEFORE UPDATE ON posts BEGIN
-  UPDATE posts SET updated_at = CURRENT_TIMESTAMP WHERE id = new.id;
-END;
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require ('express');
 var app = express();
-var swlite3 = require('sqlite3').verbose;
-var db = new sqlite3.Database('itsnatsblog.db');
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('blog.db');
 var ejs = require('ejs');
 app.set('view_engine', 'ejs');
 var bodyParser = require('body-parser');
@@ -10,9 +10,29 @@ var methodOverride = require('method-override');
 app.use(methodOverride('_method'));
 
 app.get('/', function(req,res){
-	res.redirect('/itsnatsblog')
+	res.redirect('/blogs')
 });
 
-app.get('/itsnatsblog', function(req,res){
+app.get('/blogs', function(req,res){
+	db.all("SELECT * FROM posts", function(err,data){
+		if (err) {
+			console.log(err);
+		}else{
+			var posts = data;
+			console.log(posts);
+		}
+		res.render("index.ejs", {post : posts});
+	});
 
-})
+});
+
+
+
+
+
+
+
+
+
+app.listen(3000);
+console.log("Listening 3000")
